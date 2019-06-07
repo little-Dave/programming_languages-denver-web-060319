d7697ecf90bb52a0bf07ce6d7220336a50cde92a
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-06-07 15:09:42 -0600

    Reformat given hash
    Create a new hash that contains all the information of the given hash, but in an updated format; utilize Ruby class methods to accomplish

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require "pry"
+
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.map do |style, language_array|
+    language_array.map do |key, value|
+      value[:style] = [style]
+      unless new_hash.keys.include?(key)
+        new_hash[key] = value
+      else
+        new_hash[key][:style].push(value[:style][0])
+      end
+    end
+  end
+  new_hash
 end
